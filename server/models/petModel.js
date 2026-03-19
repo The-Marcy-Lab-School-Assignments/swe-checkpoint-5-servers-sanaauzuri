360d99b6b7488bf54b4d5ece7c27d5d03bc0dc2c
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2026-03-19 11:51:37 -0400

    Step 2

--- server/models/petModel.js
+++ server/models/petModel.js
@@ -5,31 +5,52 @@ const getId = () => id++;
 // TODO: Create an in-memory "database" array with 2-3 starter pets.
 // Each pet should have an id (from getId) and a name.
 
+const pets = [
+  { id: getId(), name: 'Winnie the Pooh' },
+  { id: getId(), name: 'Perry the Platypus' },
+  { id: getId(), name: 'Tom Cat' },
+];
 
 
 // TODO: Create a new pet and add it to the array. Return the new pet.
 module.exports.create = (name) => {
-
+    const newPet = { id: getId(), name };
+    todos.push(newPet);
+    return { ...newPet };
 }
 
 // TODO: Return a copy of all pets.
 module.exports.list = () => {
-
+    return [...pets];
 }
 
 // TODO: Find and return a single pet by its ID.
 module.exports.find = (id) => {
-
+    const pet = pets.find((pet) => pet.id === id);
+    if (!pet) {
+        return null;
+    }
+    return { ...pet };
 }
 
 // TODO: Update the name of a pet. Return the updated pet,
 // or null if not found.
 module.exports.update = (id, newName) => {
-
+    const pet = pets.find((pet) => pet.id === id);
+    if (!pet) {
+        return null;
+    }
+    pet.name = newName.name;
+    return { ...todo };
 }
 
 // TODO: Remove a pet from the array. Return the deleted pet,
 // or null if not found.
 module.exports.destroy = (id) => {
-
+    const petIndex = pets.findIndex((pet) => pet.id === id);
+    if (petIndex < 0) {
+        return false;
+    }
+    pets.splice(petIndex, 1);
+    return true;
 }
