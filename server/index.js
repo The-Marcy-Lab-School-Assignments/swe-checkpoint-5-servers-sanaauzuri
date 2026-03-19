76139595b55547e8261ba8f3dc348dc449e8738f
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2026-03-19 11:35:46 -0400

    Step 1

--- server/index.js
+++ server/index.js
@@ -12,13 +12,19 @@ const app = express();
 
 // TODO: Create a logRoutes middleware function that logs the method and
 // originalUrl of every incoming request, along with the current time.
-
+const logRoutes = (req, res, next) => {
+  const time = (new Date()).toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next();
+};
+app.use(logRoutes);
 
 // TODO: Add the express.json() middleware to parse JSON request bodies.
-
+app.use(express.json());
 
 // TODO: Serve the frontend/ folder as static assets using express.static()
-
+const pathToFrontend = path.join(__dirname, '../frontend');
+app.use(express.static(pathToFrontend))
 
 /////////////////////
 // Endpoints
